bce91864336f97ced401a5fba5be810885e1c818
Alberto Bitto <alberto.bitto@previnet.it>
2022-02-11 15:05:37 +0100

    import BootstrapVue

--- progetto-fermi-webapp/src/main.ts
+++ progetto-fermi-webapp/src/main.ts
@@ -4,6 +4,12 @@ import 'regenerator-runtime'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, BootstrapVueIcons , ModalPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false
 
--- progetto-fermi-webapp/src/views/About.vue
+++ progetto-fermi-webapp/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-container fluid>
+      <b-row>
+        <b-col cols="2">These are 2 Bootstrap columns</b-col>
+        <b-col cols="4">These are 4 Bootstrap columns</b-col>
+        <b-col cols="6">These are 6 Bootstrap columns</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
